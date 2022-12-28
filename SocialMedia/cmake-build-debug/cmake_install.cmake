2d745590a055bfc22ae3fc57462fce1ca7976ae1
Lucanb <66071357+Lucanb@users.noreply.github.com>
2022-12-28 18:51:24 +0200

    ide configurations
    ...

--- SocialMedia/cmake-build-debug/cmake_install.cmake
+++ SocialMedia/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/lolluckestar/Desktop/Retele/Proiect/SocialMedia
+# Install script for directory: /home/lolluckestar/Desktop/Retele_28_Dec/SocialMedia
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/lolluckestar/Desktop/Retele/Proiect/SocialMedia/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/lolluckestar/Desktop/Retele_28_Dec/SocialMedia/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
